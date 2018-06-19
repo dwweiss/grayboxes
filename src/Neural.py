4092c879e75a9947102d57510c1e2836b790033b
Dietmar W. Weiss <dwweiss@users.noreply.github.com>
2018-06-19 11:11:16 +0200

    Update and rename grayBoxes/Neural.py to src/Neural.py

--- src/Neural.py
+++ src/Neural.py
@@ -17,7 +17,7 @@
   02110-1301 USA, or see the FSF site: http://www.fsf.org.
 
   Version:
-      2018-06-03 DWW
+      2018-06-19 DWW
 
   Acknowledgements:
       Neurolab is a contribution by E. Zuev (pypi.python.org/pypi/neurolab)
